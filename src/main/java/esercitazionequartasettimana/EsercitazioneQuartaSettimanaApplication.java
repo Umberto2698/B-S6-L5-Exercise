e72108aa189d2b3daa864ae4a25762759f9dccdc
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-10 09:04:19 +0100

    start

---
+++ src/main/java/esercitazionequartasettimana/EsercitazioneQuartaSettimanaApplication.java
@@ -0,0 +1,13 @@
+package esercitazionequartasettimana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneQuartaSettimanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercitazioneQuartaSettimanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/esercitazionequartasettimana/EsercitazioneQuartaSettimanaApplicationTests.java
@@ -0,0 +1,13 @@
+package esercitazionequartasettimana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneQuartaSettimanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
